38cc8ca5566a248aaa7b127a602d70c41d097512
re4ever <re4ever@naver.com>
2021-04-25 21:57:57 +0900

    Update

--- Translate.bat
+++ Translate.bat
@@ -5,10 +5,11 @@ mkdir Result
 mkdir Result\Data
 mkdir Result\Resources
 
-dir/b .\Data > loader
-dir/b .\Resources > resourcesLoader
+dir/b .\Data > TextFiles.txt
+dir/b .\Resources > UIFiles.txt
+dir/b .\Font > FontFiles.txt
 
-.\bin\CollectString.exe loader resourcesLoader ArteryGearCN2KOR.csv
+.\bin\CollectString.exe TextFiles.txt UIFiles.txt ArteryGearCN2KOR.csv
 
 copy .\Font\ui_font_zh_content .\Result\Resources\ui_font_zh_content
 copy .\Font\ui_font_zh_title .\Result\Resources\ui_font_zh_title
