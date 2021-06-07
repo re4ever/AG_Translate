99d0dfb2daf0441d113d8659d7e439cedbbac4fd
re4ever <re4ever@naver.com>
2021-06-07 20:56:03 +0900

    2021-06-07 Update

--- Translator/Translate_Text.bat
+++ Translator/Translate_Text.bat
@@ -7,6 +7,7 @@ dir/b .\Data > FullTextFiles.txt
 .\bin\ArteryGearTranslator.exe Make
 .\bin\ArteryGearTranslator.exe Char FileData.txt ..\CharSheet.csv
 .\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv
+.\bin\ArteryGearTranslator.exe Load FileData.txt ..\LoadingSheet.csv
 .\bin\ArteryGearTranslator.exe I18n FileData.txt ..\TextSheet.csv ..\CharSheet.csv
 
 del /Q ..\Data\*.*
--- Translator/Update_Text.bat
+++ Translator/Update_Text.bat
@@ -5,6 +5,7 @@ del /S /Q Result\Data
 
 .\bin\ArteryGearTranslator.exe Char FileData.txt ..\CharSheet.csv
 .\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv
+.\bin\ArteryGearTranslator.exe Load FileData.txt ..\LoadingSheet.csv
 .\bin\ArteryGearTranslator.exe I18n FileData.txt ..\TextSheet.csv ..\CharSheet.csv
 
 del /Q ..\Data\*.*
