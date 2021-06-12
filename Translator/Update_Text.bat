ff103670cdc7f86986bb437378492c9b0d278472
re4ever <re4ever@naver.com>
2021-06-12 22:34:35 +0900

    텍스트가 적용 안되던 것 추가

--- Translator/Update_Text.bat
+++ Translator/Update_Text.bat
@@ -6,7 +6,10 @@ del /S /Q Result\Data
 .\bin\ArteryGearTranslator.exe Char FileData.txt ..\CharSheet.csv
 .\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv
 .\bin\ArteryGearTranslator.exe Load FileData.txt ..\LoadingSheet.csv
-.\bin\ArteryGearTranslator.exe I18n FileData.txt ..\TextSheet.csv ..\CharSheet.csv
+.\bin\ArteryGearTranslator.exe Target FileData.txt ..\TargetSheet.csv
+.\bin\ArteryGearTranslator.exe CombatCond FileData.txt ..\CombatConditionSheet.csv
+.\bin\ArteryGearTranslator.exe Voice FileData.txt ..\CharacterVoiceSheet.csv
+.\bin\ArteryGearTranslator.exe I18n FileData.txt ..\TextSheet.csv ..\CharSheet.csv ..\TargetSheet.csv ..\CombatConditionSheet.csv ..\CharacterVoiceSheet.csv
 
 del /Q ..\Data\*.*
 copy .\Result\Data\*.* ..\Data\*.*
