63b1d25b3e76d481779414c231053e5445c3ce7c
re4ever <re4ever@naver.com>
2021-06-12 14:53:57 +0900

    디크립트 수정

--- Translator/Translate_Text.bat
+++ Translator/Translate_Text.bat
@@ -4,7 +4,7 @@ del /S /Q Result\Data
 del /S /Q Result\DecryptData
 dir/b .\Data > FullTextFiles.txt
 
-.\bin\ArteryGearTranslator.exe Make
+.\bin\ArteryGearTranslator.exe Make FullTextFiles.txt FileData.txt
 .\bin\ArteryGearTranslator.exe Char FileData.txt ..\CharSheet.csv
 .\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv
 .\bin\ArteryGearTranslator.exe Load FileData.txt ..\LoadingSheet.csv
