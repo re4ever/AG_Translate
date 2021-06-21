6664f2e72c8db0de3d99a4eede9ec020fcdd71bf
re4ever <re4ever@naver.com>
2021-06-21 20:05:24 +0900

    Revert "2021-06-21 파일파싱및 적용방식 변경"
    This reverts commit 688aa84f625f76b404480c920894c2d6813b650c.

--- Translator/Translate_Text.bat
+++ Translator/Translate_Text.bat
@@ -5,9 +5,13 @@ del /S /Q Result\DecryptData
 dir/b .\Data > FullTextFiles.txt
 
 .\bin\ArteryGearTranslator.exe Make FullTextFiles.txt FileData.txt
-.\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv ..\PlotCharSheet.csv
+.\bin\ArteryGearTranslator.exe Char FileData.txt ..\CharSheet.csv
+.\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv
 .\bin\ArteryGearTranslator.exe Load FileData.txt ..\LoadingSheet.csv
-.\bin\ArteryGearTranslator.exe I18n FileData.txt ..\I18n.csv
+.\bin\ArteryGearTranslator.exe Target FileData.txt ..\TargetSheet.csv
+.\bin\ArteryGearTranslator.exe CombatCond FileData.txt ..\CombatConditionSheet.csv
+.\bin\ArteryGearTranslator.exe Voice FileData.txt ..\CharacterVoiceSheet.csv
+.\bin\ArteryGearTranslator.exe I18n FileData.txt ..\TextSheet.csv ..\CharSheet.csv ..\TargetSheet.csv ..\CombatConditionSheet.csv ..\CharacterVoiceSheet.csv
 
 del /Q ..\Data\*.*
 copy .\Result\Data\*.* ..\Data\*.*
