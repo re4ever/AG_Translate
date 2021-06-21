7a57b6d86406403cf20b0e6ae11d1a874814d816
re4ever <re4ever@naver.com>
2021-06-21 23:48:19 +0900

    2021-06-21 파일파싱 및 적용 변경

--- Translator/Update_Text.bat
+++ Translator/Update_Text.bat
@@ -3,13 +3,9 @@
 dir/b .\Result\Data > TextFiles.txt
 del /S /Q Result\Data
 
-.\bin\ArteryGearTranslator.exe Char FileData.txt ..\CharSheet.csv
-.\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv
+.\bin\ArteryGearTranslator.exe Plot FileData.txt ..\PlotSheet.csv ..\PlotCharSheet.csv ..\PlotOptionSheet.csv
 .\bin\ArteryGearTranslator.exe Load FileData.txt ..\LoadingSheet.csv
-.\bin\ArteryGearTranslator.exe Target FileData.txt ..\TargetSheet.csv
-.\bin\ArteryGearTranslator.exe CombatCond FileData.txt ..\CombatConditionSheet.csv
-.\bin\ArteryGearTranslator.exe Voice FileData.txt ..\CharacterVoiceSheet.csv
-.\bin\ArteryGearTranslator.exe I18n FileData.txt ..\TextSheet.csv ..\CharSheet.csv ..\TargetSheet.csv ..\CombatConditionSheet.csv ..\CharacterVoiceSheet.csv
+.\bin\ArteryGearTranslator.exe I18n FileData.txt ..\I18n.csv
 
 del /Q ..\Data\*.*
 copy .\Result\Data\*.* ..\Data\*.*
